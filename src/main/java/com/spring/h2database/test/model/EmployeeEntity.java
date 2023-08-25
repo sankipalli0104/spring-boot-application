0f31701c4c7083d43bc9bbce0695e9474e9d5047
sankipalli0104 <37557908+sankipalli0104@users.noreply.github.com>
2023-08-25 08:25:34 -0600

    pom updated

--- src/main/java/com/spring/h2database/test/model/EmployeeEntity.java
+++ src/main/java/com/spring/h2database/test/model/EmployeeEntity.java
@@ -1,5 +1,5 @@
 package com.spring.h2database.test.model;
-
+import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Data
 @Table(name="TBL_EMPLOYEES")
 public class EmployeeEntity {
 
@@ -24,41 +25,4 @@ public class EmployeeEntity {
     @Column(name="email", nullable=false, length=200)
     private String email;
     
-    public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-    @Override
-    public String toString() {
-        return "EmployeeEntity [id=" + id + ", firstName=" + firstName + 
-                ", lastName=" + lastName + ", email=" + email   + "]";
-    }
 }
